995ed221bc51a841e190db15cc51bb2365cde921
ujwal2003 <joshiujwal2003@gmail.com>
2023-07-31 16:55:08 -0500

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    let sum = 0;
 
+    if((typeof begin != 'number' || typeof end != 'number') || (begin < 0 || end < 0))
+        return 'ERROR';
+
+    if(end < begin)
+        [begin, end] = [end, begin]
+
+    let i = begin + 1;
+    while(i != end) {
+        sum += i;
+        i++;
+    }
+
+    return begin + sum + end;
 };
 
 // Do not edit below this line
