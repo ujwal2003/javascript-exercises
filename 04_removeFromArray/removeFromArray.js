209cbfc12ff161d4f119a970c0edba6bd10f7732
ujwal2003 <joshiujwal2003@gmail.com>
2023-07-31 16:24:13 -0500

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for(let arg of args) {
+        let index = arr.indexOf(arg);
+        if(index != -1)
+            arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
