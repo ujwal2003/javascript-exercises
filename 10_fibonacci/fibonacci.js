04f2ff12fccae7003954c041af6503ad00ecde80
ujwal2003 <joshiujwal2003@gmail.com>
2023-08-02 23:47:27 -0500

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if(num < 0) return "OOPS";
+    if(num === 0) return 0;
 
+    let prev = 1;
+    let secondPrev = 0;
+
+    for(let i=2; i <= num; i++) {
+        let curr = prev + secondPrev;
+        secondPrev = prev;
+        prev = curr;
+    }
+
+    return prev;
 };
 
 // Do not edit below this line
