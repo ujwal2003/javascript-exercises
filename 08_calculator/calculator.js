b34b425a2179e77507a38710ad80a2789539d4b2
ujwal2003 <joshiujwal2003@gmail.com>
2023-08-02 22:32:56 -0500

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	for(num of nums) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let product = 1;
+	for(num of nums) {
+    product *= num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num <= 1)
+    return 1;
+
+  let solution = 1;
+  for(let i=num; i>=1; i--)
+    solution *= i;
+
+  return solution;
 };
 
 // Do not edit below this line
