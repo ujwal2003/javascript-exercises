b753a088541a22984cafbdd120dda20839bbaa3d
ujwal2003 <joshiujwal2003@gmail.com>
2023-08-02 23:40:45 -0500

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    reversed = str.split('').reverse().join('');
+    return str === reversed;
 };
 
 // Do not edit below this line
