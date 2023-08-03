f61dcbe7bdec2f330d2677ce2ff817756fa464b6
ujwal2003 <joshiujwal2003@gmail.com>
2023-08-03 00:10:34 -0500

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((oldest, currPerson) => {
+        const oldestAge = calculateAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currAge = calculateAge(currPerson.yearOfBirth, currPerson.yearOfDeath);
 
+        return oldestAge < currAge ? currPerson : oldest;
+    });
 };
+
+const calculateAge = function(birthYear, deathYear) {
+    if(!deathYear)
+        deathYear = new Date().getFullYear();
+
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
